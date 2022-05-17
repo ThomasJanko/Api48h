6b6ed6443c9a18dce1828302c4df1b3550072cf2
thomas78125 <thomas78125@gmail.com>
2022-05-17 14:59:58 +0200

    app key

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: '6eLpWZ4vtZ3Bt841wTM/dg==,HQnN6fNSP4a5QECVDtXG/w==,pYLRp70m0mVSmaqYZ9xMHQ==,4ngfpt+T+5zyL5+g4ZyE/A=='
   },
 });
